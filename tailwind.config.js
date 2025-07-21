99e66e0f01a9dbb5065a11e2ed1de125e204b2c7
Lampard Vergiawan Sasriano <vergilagi@gmail.com>
2025-07-22 05:27:06 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./index.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["'Share Tech Mono'", "monospace"],
+      },
+    },
+  },
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        neon: "#39ff14",
+        darkBlue: "#0f172a",
+        cyberPink: "#ff007c",
+        cyberBlue: "#00eaff",
+      },
+    },
+  },
+  plugins: [],
+}
+
